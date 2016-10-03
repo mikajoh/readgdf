23c20438be09156ada95bad16912accbd58ac7d7
MIkael Poul Johannesson <mikajoh@gmail.com>
2016-10-03 02:05:40 +0200

    Wrkd on speed improvments, incl use stringr+readr; also updt docs

--- R/read_gdf.R
+++ R/read_gdf.R
@@ -1,39 +1,53 @@
-#' Read in a gdf file and return an igraph data frame
+#' Read in a gdf file
 #'
-#' Read in a gdf file and return an igraph data frame
+#' Read in a gdf file and return an igraph graph
 #'
 #' @importFrom igraph graph_from_data_frame
+#' @importFrom stringr str_detect
+#' @importFrom readr read_lines
 #' @importFrom data.table fread
 #' @importFrom bit64 integer64
 #'
 #' @param filepath File to import
-#' @param as_igraph Return as igraph graph. If \code{FALSE}, it returns a list with the node and edge data, respectively.
-#' @param verbose Whether to print progress in console.
+#' @param as_igraph Return as igraph graph. If \code{FALSE} it returns a list with the node and edge data, respectively, instead.
+#' @param verbose Whether to print progress in the console.
 #' @export
-read_gdf <- function(filepath, as_igraph = TRUE, verbose = TRUE) {
+read_gdf <- function(filepath, as_igraph = TRUE, verbose = FALSE) {
   if (verbose) message("1: Reading in raw file")
-  gdf <- readLines(filepath, warn = FALSE)
+  gdf <- readr::read_lines(filepath, progress = verbose)
   if (verbose) message("2: Extracting edge data")
-  edge_place_l <- grepl("edgedef>", gdf)
+  edge_place_l <- stringr::str_detect(gdf, "edgedef>")
   has_edge_data <- any(edge_place_l)
   if (has_edge_data) {
     edge_place <- which(edge_place_l)
     node_data <- gdf[1:(edge_place - 1)]
     edge_data <- gdf[edge_place:length(gdf)]
-    edge_data <- gsub("edgedef>node", "node", edge_data)
+    edge_data[1] <- gsub("edgedef>node", "node", edge_data[1])
     edge_data[1] <- paste0(sapply(strsplit(edge_data[1], ","), function(x) gsub("^(.*) [A-Z]+$", "\\1", x)), collapse = ",")
-    edge_data_collapsed <- paste0(edge_data, collapse = "\n")
-    edge_data <- as.data.frame(data.table::fread(edge_data_collapsed, sep = ",", header = TRUE))
+    edge_data <- paste0(edge_data, collapse = "\n")
+    edge_data <- data.table::fread(edge_data,
+                                   sep = ",",
+                                   header = TRUE,
+                                   data.table = FALSE,
+                                   integer64 = "double",
+                                   verbose = verbose,
+                                   showProgress = verbose)
   } else {
     if (verbose) message("(Didn't find edge data)")
     node_data <- gdf
     edge_data <- ""
   }
   if (verbose) message("3: Extracting node data")
-  node_data <- gsub("nodedef>name", "name", node_data)
+  node_data[1] <- gsub("nodedef>name", "name", node_data[1])
   node_data[1] <- paste0(sapply(strsplit(node_data[1], ","), function(x) gsub("^(.*) [A-Z]+$", "\\1", x)), collapse = ",")
-  node_data_collapsed <- paste0(node_data, collapse = "\n")
-  node_data <- as.data.frame(data.table::fread(node_data_collapsed, sep = ",", header = TRUE))
+  node_data <- paste0(node_data, collapse = "\n")
+  node_data <- data.table::fread(node_data,
+                                 sep = ",",
+                                 header = TRUE,
+                                 data.table = FALSE,
+                                 integer64 = "double",
+                                 verbose = verbose,
+                                 showProgress = verbose)
   node_data <- node_data[-c(which(duplicated(node_data$name))), ]  # Remove duplicated nodes
   if (as_igraph & has_edge_data) {
     if (verbose) message("4: Converting to igraph")
--- README.md
+++ README.md
@@ -1,13 +1,14 @@
-# readgdf
+readgdf: Read gdf files
+---------------
 
 [![Build Status](https://travis-ci.org/mikaelpoul/readgdf.svg?branch=master)](https://travis-ci.org/mikaelpoul/readgdf)
 
-Extract network data from gdf files and import it into R.
+Read gdf files into R as igraph graphs. Work in progress. May or may not work for you. If not, please open an issue or [email me](mailto:mikajoh@gmail.com).
 
 Install the package using devtools (use `install.packages("devtools")` if you don't have it installed):
 
 ``` R
-devtools::install_github("mikaelpoul/readgdf", dependencies = TRUE)
+devtools::install_github("mikaelpoul/readgdf")
 ```
 
 Then simply load the package and read your gdf file with `read_gdf`:
@@ -17,7 +18,4 @@ library(readgdf)
 data <- read_gdf("path/to/gdf_file.gdf")
 ```
 
-It will return an igraph graph object by default, but you can set `as_igraph = FALSE` to make it return a list with the node and edge data, respectively. it is `verbose = TRUE` by default.
-
-
-
+It will return an igraph graph object by default, but you can set `as_igraph = FALSE` to make it return a list with the node and edge data, respectively, instead. To print the progress while reading set `verbose = TRUE`.
